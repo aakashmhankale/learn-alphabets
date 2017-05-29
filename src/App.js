e50e3ee64e6e0b08dfee4d7d68cb430bb465a21e
Aakash <aakashmhankale@gmail.com>
2017-05-28 20:28:53 -0700

    react-create-app on heroku

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import EasyABC from './EasyABC'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Learn-Alpha</h2>
+        </div>
+        <EasyABC />
+      </div>
+    );
+  }
+}
+
+export default App;
